32643510750e7acee6d4144360b32b24e7b220cb
Victor <main.for.work.adress@gmail.com>
2026-01-08 10:45:37 +0300

    edit routes

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/React-Quiz-App",
+  base: "/React-Quiz-App/",
 })
